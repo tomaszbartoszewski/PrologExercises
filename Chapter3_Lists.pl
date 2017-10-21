a56b87a0e00190e6a1ce53d663800f7d908a38a0
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2017-10-21 12:10:10 +0100

    Car exercise with list, consumption

--- Chapter3_Lists.pl
+++ Chapter3_Lists.pl
@@ -36,3 +36,37 @@ change(H, H).
 
 alter([],[]).
 alter([H|T], [HNew|TNew]) :- change(H, HNew), alter(T, TNew).
+
+
+
+fuel_consumed(waster, [3.1, 10.4, 15.9, 10.3]).
+fuel_consumed(guzzler, [3.2, 9.9, 13.0, 11.6]).
+fuel_consumed(prodigal, [2.8, 9.8, 13.1, 10.4]).
+
+equal_or_better_consumption(Good, Bad) :-
+        Threshold is (Good + Bad) / 40,
+        Worst is Bad + Threshold,
+        Good < Worst.
+
+prefer(Car1, Car2) :-
+        fuel_consumed(Car1, Con1),
+        fuel_consumed(Car2, Con2),
+        always_better(Con1, Con2).
+
+always_better([], []).
+always_better([H1|T1], [H2|T2]) :- equal_or_better_consumption(H1, H2), always_better(T1, T2).
+
+prefer_sometimes(Car1, Car2) :-
+        fuel_consumed(Car1, Con1),
+        fuel_consumed(Car2, Con2),
+        sometimes_better(Con1, Con2).
+
+sometimes_better([H1|_], [H2|_]) :-
+        significantly_better(H1, H2).
+sometimes_better([_|T1], [_|T2]) :-
+        sometimes_better(T1, T2).
+
+significantly_better(Good, Bad) :-
+        Threshold is (Good + Bad) / 40,
+        BetterThan is Bad - Threshold,
+        Good < BetterThan.
