5fdfe81135b6fda7ee6affef7de0c9b8ef82043c
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2017-12-12 22:33:53 +0000

    Chapter 7 - searching graphs

---
+++ Chapter7_graphs.pl
@@ -0,0 +1,30 @@
+a(g,h).
+a(g,d).
+a(e,d).
+a(h,f).
+a(e,f).
+a(a,e).
+a(a,b).
+a(b,f).
+a(b,c).
+a(f,c).
+a(d,a).
+
+
+% go(a, d).
+go(X, X).
+go(X, Y) :- a(X, Z), go(Z, Y).
+
+
+% go(a, d, []).
+go(X, X, _).
+go(X, Y, T) :- a(X, Z), legal(Z, T), go(Z, Y, [Z|T]).
+
+legal(_, []).
+legal(X, [H|T]) :- \+ X = H, legal(X, T).
+
+
+goUndirected(X, X, _).
+goUndirected(X, Y, T) :-
+    (a(X, Z) ; a(Z, X)),
+    legal(Z, T), go(Z, Y, [Z|T]).
