b511cb1a145c72633f4795c834a30ad224cfa895
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2017-12-21 20:10:49 +0000

    Chapter 7 - Retractall implementation

--- Chapter7_manipulatingPrograms.pl
+++ Chapter7_manipulatingPrograms.pl
@@ -32,3 +32,46 @@ test_int(a) :- test_int(b).
 test_int(b) :- test_int(c).
 test_int(c).
 test_int(d).
+
+
+
+% asserta(testx), asserta(testx :- testy), list1(testx).
+% retractall_imp(testx), list1(testx).
+
+retractall_imp(X) :- retract(X), fail.
+retractall_imp(X) :- retract((X :- _)), fail.
+retractall_imp(_).
+
+
+%consult_imp(File) :-
+%    retractall(done(_)),
+%    current_input(Old),
+%    open(File, read, Stream),
+%    repeat,
+%    read(Term),
+%    try(Term),
+%    close(Stream),
+%    set_input(Old),
+%    !.
+%
+%try(end_of_file) :- !.
+%try((?- Goals)) :- call(Goals), !, fail.
+%try((:- _)) :- !.
+%try(Clause) :-
+%    head(Clause, Head),
+%    record_done(Head),
+%    assertz(Clause),
+%    fail.
+%
+%:- dynamic done/1.
+%
+%record_done(Head) :- done(Head), !.
+%record_done(Head) :-
+%    functor(Head, Func, Arity),
+%    functor(Proc, Func, Arity),
+%    asserta(done(Proc)),
+%    retractall(Proc),
+%    !.
+%
+%head((A :- _), A) :- !.
+%head(A, A).
